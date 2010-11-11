3409e1eb3ce9d4e84879fc654daf803812fd52b1
Damian Janowski <damian.janowski@gmail.com>
2010-11-11 16:44:30 -0300

    Fix authorization URL.

--- src/mercadolibre.js
+++ src/mercadolibre.js
@@ -2,7 +2,7 @@
 
 window.MercadoLibre = {
   baseURL: "https://api.mercadolibre.com",
-  authorizationURL: "http://oauth.mercadolibre.com/jms/mla/authorization",
+  authorizationURL: "http://auth.mercadolibre.com/jms/mla/authorization",
 
   hash: {},
   callbacks: {},
