3e8e06a714d2d7fd26270d0e58a5edb73d27bed4
Dario Guzik <dario.guzik@mercadolibre.com>
2012-08-02 13:55:16 -0300

    url fix

--- pkg/mercadolibre.js
+++ pkg/mercadolibre.js
@@ -2210,7 +2210,7 @@ var XAuth = (function () {
 		_initXAuthClient: function() {
                         this.options.xauth_domain = this.options.xauth_domain || "static.mlstatic.com";
                         this.options.auth_timeout = this.options.auth_timeout || 3000;
-                        this.options.xd_url = this.options.xd_url || "/org-img/sdk/xd_20120224-chico.html";
+                        this.options.xd_url = this.options.xd_url || "/org-img/sdk/xd-1.0.2.html";
                         this.options.xauth_protocol = this.options.xauth_protocol || "http://";
 
 			if(this.options.xauth_domain_fallback && !this.messages)
